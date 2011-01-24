be6564b06eee13a2fb6c55c67c689fc5f1caf687
Mihai Rotaru <mihai_rotaru@lavabit.com>
2011-01-24 10:00:03 +0000

    now works for archives with names containing spaces

--- hooks/post-Firefox.bat
+++ hooks/post-Firefox.bat
@@ -3,5 +3,5 @@
 IF EXIST "%TARGET_DIR%%PACKAGE_TARGET_DIR%\%PACK_NAME%\FirefoxPortable.exe" (
 ECHO Firefox found, creating shortcut...
 %BSD%\shortcut /A:C /T:"%TARGET_DIR%%PACKAGE_TARGET_DIR%\%PACK_NAME%\FirefoxPortable.exe" /F:^"^%USERPROFILE%\Desktop\Firefox.lnk^" /R:3
-)
 ECHO shortcut.exe returned %ERRORLEVEL%
+)
--- hooks/post-gVim.bat
+++ hooks/post-gVim.bat
@@ -3,5 +3,5 @@
 IF EXIST "%TARGET_DIR%%PACKAGE_TARGET_DIR%\%PACK_NAME%\gVimPortable.exe" (
 ECHO gVim found, creating shortcut...
 %BSD%\shortcut /A:C /T:"%TARGET_DIR%%PACKAGE_TARGET_DIR%\%PACK_NAME%\gVimPortable.exe" /F:^"^%USERPROFILE%\Desktop\gVim.lnk^" /R:3
-)
 ECHO shortcut.exe returned %ERRORLEVEL%
+)
