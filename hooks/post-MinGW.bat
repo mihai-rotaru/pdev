2fb7170b75168eddcaec47d89dcd0c7621fa7a11
Mihai Rotaru <mihai_rotaru@lavabit.com>
2011-02-09 13:57:47 +0000

    git and MinGW hooks copy .rc files

--- hooks/post-MinGW.bat
+++ hooks/post-MinGW.bat
@@ -1,1 +1,5 @@
 %BSD%\setx PATH "C:\pdev\MinGW\bin;C:\pdev\MinGW\msys\1.0\bin;C:\pdev\apps\gVim\Tools;C:\pdev\MinGW\tools;C:\Python26;C:\ruby-1.9.1\bin;c:\pdev\apps\7-Zip\App\7-Zip;C:\Python26\Scripts;c:\pdev\git\bin;C:\pdev\apps\gVim\App\vim\vim73"
+
+IF EXIST "%PDEV_ROOT%\files\.bashrc" (
+ECHO found .bashrc on your deveice...
+XCOPY "%PDEV_ROOT%\files\.bashrc" "%USERPROFILE%\.bashrc" /I /-Y
